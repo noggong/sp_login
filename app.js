da8e1b475b6143dd13c7007b06a282092ef7df8c
우준호 <noggong@gmail.com>
2022-12-29 20:31:09 +0900

    routing 생성

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express")
 
 const app = express()
+
+app.post('/login', (req, res) => {
+    res.send("로그인")
+})
+
+
+app.post('/logout', (req, res) => {
+    res.send("로그아웃")
+})
+
+app.post('/register', (req, res) => {
+    res.send("회원 가입")
+})
+
+app.get('/users', (req, res) => {
+    res.send("회원 정보")
+})
+
+
 app.listen(3000, () => {
     console.log("서버 실행")
 })
+
